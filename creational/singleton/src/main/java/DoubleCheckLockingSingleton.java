7c5a3bc49f8535d41661f03f7021faccad344288
方斌 <a1678566105@163.com>
2022-10-29 05:26:57 +0800

    双重校验锁注释

--- creational/singleton/src/main/java/DoubleCheckLockingSingleton.java
+++ creational/singleton/src/main/java/DoubleCheckLockingSingleton.java
@@ -1,13 +1,16 @@
 //双重校验锁，线程安全，效率高
 public class DoubleCheckLockingSingleton {
 
+    //禁止指令重排序
     private static volatile DoubleCheckLockingSingleton instance;
 
     private DoubleCheckLockingSingleton(){};
 
     public static DoubleCheckLockingSingleton getInstance () {
+        //返回单例，避免多余的加锁操作
         if(instance == null){
             synchronized (DoubleCheckLockingSingleton.class){
+                //防止多线程创建多个实例
                 if(instance == null){
                     instance = new DoubleCheckLockingSingleton();
                 }
