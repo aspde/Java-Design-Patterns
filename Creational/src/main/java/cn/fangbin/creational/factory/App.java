e4a649e863cc4a76c92dce10a591f43250601cca
aspde <1678566105@qq.com>
2023-04-28 16:30:03 +0800

    修改日志

--- Creational/src/main/java/cn/fangbin/creational/factory/App.java
+++ Creational/src/main/java/cn/fangbin/creational/factory/App.java
@@ -6,10 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 public class App {
 
     public static void main(String[] args) {
-        log.info("硬币工厂开始生产");
+        LOGGER.info("硬币工厂开始生产");
         Coin coin1 = CoinFactory.getCoin(CoinType.COLD);
         Coin coin2 = CoinFactory.getCoin(CoinType.COPPER);
-        log.info(coin1.getDescription());
-        log.info(coin2.getDescription());
+        LOGGER.info(coin1.getDescription());
+        LOGGER.info(coin2.getDescription());
     }
 }
